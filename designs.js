932acbae0c1d1f113c19dc499c3d8718d231d1ec
tomstamand <52722667+tomstamand@users.noreply.github.com>
2019-07-09 21:24:41 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -2,7 +2,6 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
 const color = document.querySelector('#colorPicker');
 const canvas = document.querySelector('#pixelCanvas');
 const sizePicker = document.querySelector('#sizePicker');
@@ -19,15 +18,16 @@ sizePicker.addEventListener('submit', (e) => {
     canvas.firstElementChild.remove();
     makeGrid(height, width);
 });
+
 function makeGrid(high, wide) {
-for (let h = 0; h < high; h++) {
-    let col = canvas.insertRow(h);
-    for (let w = 0; w < wide; w++) {
-        let row = col.insertCell(releaseEvents);
-        //colors cells
-        row.addEventListener('click', (e) => {
-            row.style.backgroundColor = color.value;
-        });
+    for (let h = 0; h < high; h++) {
+        let col = canvas.insertRow(h);
+        for (let w = 0; w < wide; w++) {
+            let row = col.insertCell(releaseEvents);
+            //colors cells
+            row.addEventListener('click', (e) => {
+                row.style.backgroundColor = color.value;
+            });
+        }
     }
-}
 }
